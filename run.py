fc8a3b7b8ade1c89fb4b059d0d77fac6aa2f5f31
upeavcr <upeavcr@gmail.com>
2024-12-30 19:43:25 -0400

    Leccion 02 - Segunda fase uso de plantillas html

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
-from flask import Flask, url_for
-#from flask import url_for
+from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
@@ -7,18 +6,19 @@ posts = []
 
 @app.route("/")
 def index():
-    return "{} posts".format(len(posts))
+    return render_template("index.html", num_posts=len(posts))
+
 
 @app.route("/p/<string:slug>/")
 def show_post(slug):
-    return "Mostrando el post {}".format(slug)
+    return render_template("post_view.html", slug_title=slug)
+
 
 @app.route("/admin/post/")
-@app.route("/admin/post/<int:post_id>")
+@app.route("/admin/post/<int:post_id>/")
 def post_form(post_id=None):
-    return "post_form {}".format(post_id)
+    return render_template("admin/post_form.html", post_id=post_id)
+
 
 if __name__=='__main__':
     app.run(host='localhost', port=8080, debug=True)
-    #print(url_for("index")) # /
-    #print(url_for("show_post", slug="leccion-1", preview=True)) # /p/leccion-1/?preview=True
